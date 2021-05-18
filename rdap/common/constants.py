cead3e5299ea06a3fd1af90da65082f36a4ccf54
Alexei <alexei97531@gmail.com>
2021-05-17 22:12:34 -0300

    fixes, fixes and a couple of improvements

--- rdap/common/constants.py
+++ rdap/common/constants.py
@@ -38,3 +38,4 @@ class TextFormatConstants:
     TEXT = "txt"
     JSON = "json"
     CSV = "csv"
+    YML = "yml"
--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ def get_packages() -> list:
 
 setup(
     name = "rdap",
-    version = "0.0.1",
+    version = "0.0.2",
     author = "Alexei Popov",
     author_email = "",
     entry_points = {
