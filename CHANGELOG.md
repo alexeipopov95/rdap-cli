011b86bcae0b40e74b24458f0b1782b56533f697
Alexei <alexei97531@gmail.com>
2021-06-18 22:23:45 -0300

    Black and Flake8

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,12 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 
 ## [Unreleased]
 
+## [0.1.4] - 19 Jun 2021
+
+### Change
+- Applied Black
+- Applied Flake8
+
 ## [0.1.3] - 19 Jun 2021
 
 ### Added
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 def read_requirements() -> list:
     """simple function to read the requirements
-    file and add them as a list into the 
+    file and add them as a list into the
     install_requires of the CLI
 
     Returns:
@@ -16,6 +16,7 @@ def read_requirements() -> list:
 
     return requirements
 
+
 def get_packages() -> list:
     """simple function designed to return a list
     of packages involved in our CLI
@@ -25,27 +26,25 @@ def get_packages() -> list:
     """
 
     root = ["rdap"]
-    package_list = [
-        "rdap.{0}".format(package) for package in find_packages(root[0])
-    ]
+    package_list = ["rdap.{0}".format(package) for package in find_packages(root[0])]
     return root + package_list
 
 
 setup(
-    name = "rdap",
-    version = "0.1.3",
-    author = "Alexei Popov",
-    author_email = "alexei.popov.cli@gmail.com",
-    entry_points = {
-        "console_scripts" : [
+    name="rdap",
+    version="0.1.4",
+    author="Alexei Popov",
+    author_email="alexei.popov.cli@gmail.com",
+    entry_points={
+        "console_scripts": [
             "rdap=rdap.rdap:cli",
         ]
     },
-    description= (
+    description=(
         "A simple CLI designed to offer a quick way of gathering domain"
         "data information."
     ),
     python_requires=">=3.6",
-    install_requires = read_requirements(),
-    packages = get_packages(),
+    install_requires=read_requirements(),
+    packages=get_packages(),
 )
