17002be1e1d1396f6f61bbe663e7bcc8b95c6431
Alexei <alexei97531@gmail.com>
2021-06-18 23:26:15 -0300

    edited setup

--- setup.py
+++ setup.py
@@ -1,22 +1,6 @@
 from setuptools import setup, find_packages
 
 
-def read_requirements() -> list:
-    """simple function to read the requirements
-    file and add them as a list into the
-    install_requires of the CLI
-
-    Returns:
-        list: [list of libs that the CLI needs]
-    """
-
-    with open("requirements.txt") as req:
-        content = req.read()
-        requirements = content.split("\n")
-
-    return requirements
-
-
 def get_packages() -> list:
     """simple function designed to return a list
     of packages involved in our CLI
@@ -50,7 +34,18 @@ setup(
     ),
     keywords=["rdap", "cli", "whois", "registration-date", "data", "domain"],
     python_requires=">=3.6",
-    install_requires=read_requirements(),
+    install_requires=[
+        "black==21.6b0",
+        "click==8.0.1",
+        "flake8==3.9.2",
+        "pre-commit==2.13.0",
+        "python-dateutil==2.8.1",
+        "PyYAML==5.4.1",
+        "requests==2.25.1",
+        "tabulate==0.8.9",
+        "tldextract==3.1.0",
+        "validators==0.18.2",
+    ],
     packages=get_packages(),
     classifiers=[
         "Intended Audience :: Developers",
